4b4e376242779a007e1ef97b3e7fa14870b71601
Kevin Grasso <kevin.grasso97@gmail.com>
2021-01-06 15:26:35 +0100

    fixed function

--- src/functions/get_position_on_list.js
+++ src/functions/get_position_on_list.js
@@ -1,7 +1,7 @@
 export function get_position(list, track_id){
     for( let el of list){
-        
-        if(el.tracking_number == track_id){
+     
+        if(el.tracking_number.toUpperCase() == track_id.toUpperCase()){
             return list.indexOf(el)
         }
     }
