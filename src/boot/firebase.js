855c63c98abe8a94fe287535f894a2b9b9512454
kevingrasso <76524026+kevingrasso@users.noreply.github.com>
2021-11-02 09:52:00 +0100

    Update firebase.js

--- src/boot/firebase.js
+++ src/boot/firebase.js
@@ -5,14 +5,14 @@ import "firebase/auth";
 import "firebase/database";
 
 var firebaseConfig = {
-  apiKey: "AIzaSyAZn-eJufwaMfPkJpRcUI1NEZO1Qbzi_B4",
-  authDomain: "order-track-2ed24.firebaseapp.com",
-  databaseURL: "https://order-track-2ed24-default-rtdb.firebaseio.com",
-  projectId: "order-track-2ed24",
-  storageBucket: "order-track-2ed24.appspot.com",
-  messagingSenderId: "177748184152",
-  appId: "1:177748184152:web:2d046d1b76dfc2a37c0e2d",
-  measurementId: "G-FMFK44RYNY"
+  apiKey: "****************************",
+  authDomain: "*****************************",
+  databaseURL: "**********************************",
+  projectId: "***********************",
+  storageBucket: "********************",
+  messagingSenderId: "************************",
+  appId: "*******************************",
+  measurementId: "***************************"
 };
 
 let firebaseApp = firebase.initializeApp(firebaseConfig);
